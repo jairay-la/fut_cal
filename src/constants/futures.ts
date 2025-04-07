f326fe1a3d84bcc284c8004a8e2cae4793883291
jairay-la <rayl.mail@gmail.com>
2025-04-07 10:09:08 -0700

    Initial commit: Futures Position Calculator

---
+++ src/constants/futures.ts
@@ -0,0 +1,7 @@
+export const POINT_VALUES = {
+  E: { value: 50, name: 'S&P 500 E-mini (ES)' },
+  R: { value: 50, name: 'Russell 2000 (RTY)' },
+  N: { value: 20, name: 'Nasdaq 100 E-mini (NQ)' },
+  C: { value: 1000, name: 'Crude Oil (CL)' },
+  G: { value: 100, name: 'Gold (GC)' }
+} as const
